ca05a6a5e69b8d5ea7cc46d8e096a5b344bfc803
AlnurZhanibek-inDrive <alnur.zhanibek@indriver.com>
2025-05-03 16:55:11 +0500

    feat: add cert url

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -6,6 +6,9 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.policyLinks}>
         <Link href="/payment-policy">Политика платежей</Link>
+        <Link href="https://kazusa-bucket.fra1.cdn.digitaloceanspaces.com/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA%20%D0%BF%D1%80%D0%B5%D0%BF%D0%BE%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B8%CC%86,%20%D0%BF%D1%80%D0%BE%D1%88%D0%B5%D0%B4%D1%88%D0%B8%D1%85%20%D0%BA%D1%83%D1%80%D1%81%20%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%8F.pdf">
+          Список успешно завершивших преподавателей
+        </Link>
       </div>
       <p>© 2024 KAZUSA. All rights reserved.</p>
     </footer>
